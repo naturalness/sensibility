9eb9ae39b89ccda15d80febaeff947099e138d4d
Eddie Antonio Santos <easantos@ualberta.ca>
2016-10-18 16:28:20 -0600

    Update enqueueing.

--- src/q/q.ts
+++ src/q/q.ts
@@ -27,9 +27,13 @@ export default class Q {
    * Push an item to the queue.
    */
   enqueue(object: string | RedisSerializable): Promise<number> {
-    const serialized =
-      (typeof object === "string") ? object : object.toRedisString();
-    return this.redis.lpushAsync(this.name, serialized);
+    return Promise.resolve()
+    .then(() => {
+        const serialized =
+          (typeof object === "string") ? object : object.toRedisString();
+
+        return this.redis.lpushAsync(this.name, serialized);
+    });
   }
 
   /**
--- test.js
+++ test.js
@@ -1,7 +1,20 @@
 const Q = require('./lib/q').default;
 const redis = require('redis');
 
+function Herp() {
+}
+Herp.prototype.toRedisString = function () {
+  throw new Error('lol');
+};
+
 const client = redis.createClient();
 const q = new Q(client, 'herp');
-q.enqueue('herp')
-  .then((n) => console.log(`Now there are ${n} items`));
+q.enqueue(new Herp)
+  .then(n => {
+    console.log(`Now there are ${n} items`)
+    client.quit();
+  })
+  .catch(err => {
+    console.error(err);
+    client.quit();
+  });
