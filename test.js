06041d3ff94e17bde118a652aad37223a4f52956
Eddie Antonio Santos <easantos@ualberta.ca>
2016-10-22 14:48:40 -0600

    Start writing test...

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-const Q = require('./lib/q').default;
-const redis = require('redis');
+const fs = require('fs');
 require('colors');
 
 function queueTest() {
   console.log('Queue test'.green);
+
+  const Q = require('./lib/q').default;
+  const redis = require('redis');
 
   function Herp() {
   }
@@ -26,7 +28,6 @@ function queueTest() {
 
 function parseTest() {
   console.log('Parse test'.green);
-  const fs = require('fs');
   const parsePipeline = require('./lib/esprima-pipeline.js').default;
 
   const file = fs.readFileSync('test.js', 'utf8');
@@ -34,4 +35,19 @@ function parseTest() {
   console.dir(result);
 }
 
-parseTest();
+function dbTest() {
+  console.log('Database test'.green);
+
+  const sqlite3 = require('sqlite3').verbose();
+  const db = new sqlite3.Database(':memory:');
+
+  const schema = fs.readFileSync('./schema.sql', 'utf8');
+
+  db.serialize(() => {
+    db.run(schema);
+  });
+
+  db.close();
+}
+
+dbTest();
