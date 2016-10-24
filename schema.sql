2395dba1c27e412ef2b2b90e0da7fb5d1efbf359
Eddie Antonio Santos <easantos@ualberta.ca>
2016-10-24 06:22:05 -0600

    Minor touch-ups.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Eddie Antonio Santos <easantos@ualberta.ca>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 -- Represents a source code repository.
 CREATE TABLE repository (
     owner       TEXT NOT NULL, -- the owner of the repository
@@ -10,11 +26,11 @@ CREATE TABLE repository (
 
 -- A single source file from a repository.
 CREATE TABLE source_file (
-    hash    TEXT PRIMARY KEY NOT NULL, -- The SHA2 hash of the file
+    hash    TEXT PRIMARY KEY NOT NULL, -- The SHA256 hash of the file
     owner   TEXT,
     repo    TEXT,
     path    TEXT NOT NULL,
-    source  BLOB NOT NULL, -- Store as raw bytes.
+    source  BLOB NOT NULL, -- Stored as raw bytes; decode on use.
 
     FOREIGN KEY (owner, repo) REFERENCES repository (owner, repo) ON DELETE CASCADE
 );
@@ -29,7 +45,7 @@ CREATE TABLE parsed_source (
     FOREIGN KEY (hash) REFERENCES source_file (hash) ON DELETE CASCADE
 );
 
--- A file is inserted here if it does not have valid syntax.
+-- A file is inserted here if it has invalid syntax.
 CREATE TABLE failure (
     hash    TEXT PRIMARY KEY
 );
