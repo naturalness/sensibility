5b4de9a9c48338ac1f0972ac563ccd39fa3a0d23
Eddie Antonio Santos <easantos@ualberta.ca>
2016-10-23 21:33:37 -0600

    Lots of implementation for downloader.

--- connection.py
+++ connection.py
@@ -7,11 +7,17 @@ Returns the appropriate clients
 
 import redis
 import sqlite3
+from github3 import login
 
-__all__ = ['redis_client', 'sqlite3_connection']
+__all__ = ['redis_client', 'sqlite3_connection', 'github']
 
 # Default connection.
 redis_client = redis.StrictRedis(db=0)
 
 # Default database
 sqlite3_connection = sqlite3.connect('parsed_sources.sqlite3')
+
+# Default GitHub
+with open('.token', 'r', encoding='UTF=8') as token_file:
+    github = login(token=token_file.read().strip())
+    del token_file
--- download_worker.py
+++ download_worker.py
@@ -7,13 +7,15 @@ Downloads metadata from the GitHub API.
 
 import logging
 import zipfile
+import datetime
+import io
 
 import requests
 
 import database
 from datatypes import RepositoryID, Repository
 from rqueue import Queue, WorkQueue
-from connection import redis_client, sqlite3_connection
+from connection import redis_client, sqlite3_connection, github
 
 QUEUE_NAME = 'q:download'
 QUEUE_ERRORS = 'q:download:aborted'
@@ -25,12 +27,11 @@ def get_repo_info(repo_id):
     url = repo_id.api_url
     logging.debug('Accessing %s', url)
 
+    wait_for_rate_limit()
     resp = repo_id.get(url, headers={
         'User-Agent': 'eddieantonio-ad-hoc-miner',
         'Accept': 'application/vnd.github.drax-preview+json'
     })
-
-    wait_for_rate_limit()
 
     assert resp.status_code == 200
     body = resp.json()
@@ -41,12 +42,45 @@ def get_repo_info(repo_id):
     return Repository(repo_id, license, rev)
 
 
+def extract_js(archive):
+    for filename in archive.namelist():
+        if not filename.endswith('.js'):
+            continue
+
+        with archive.open(filename) as js_file:
+            yield filename, js_file.read()
+
+
 def download_source_files(repo):
     assert isinstance(repo, Repository)
     url = repo.id.archive_url(format="zipball", revision=repo.revision)
+
+    wait_for_rate_limit()
     resp = requests.get(url)
-    # TODO: open zip file
-    # TODO: yield SourceFile objects
+    assert repo.status_code == 200
+
+    # Open zip file
+    fake_file = io.BytesIO(resp.content)
+    with zipfile.ZipFile(fake_file) as repo_zip:
+        # Iterate through all javascript files
+        for path, source in extract_js(repo_zip):
+            yield SourceFile.create(repo, source, path)
+
+
+def wait_for_rate_limit():
+    limit_info = github.rate_limit()
+    remaining = limit_info['core']['remaining']
+    if remaining < 10:
+        # Wait an hour
+        reset = limit_info['core']['reset']
+        time.sleep(seconds_until(reset) + 1)
+
+
+def seconds_until(timestamp):
+    now = datetime.datetime.now()
+    future = datetime.datetime.fromtimestamp(timestamp)
+    difference = future - now
+    return difference.seconds
 
 
 def main():
