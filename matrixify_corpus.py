3648aa377f2df0614636b523487aa57065279278
Eddie Antonio Santos <easantos@ualberta.ca>
2016-11-12 17:46:03 -0700

    Try not to use so much memory...

--- matrixify_corpus.py
+++ matrixify_corpus.py
@@ -2,21 +2,28 @@
 # -*- coding: UTF-8 -*-
 
 """
-Creates corpus.npz from a full corpus stored as an SQLite database. 
+Creates corpus.npz from a full corpus stored as an SQLite database.
 
 The file contains one-hot encoded matrices.
 """
+
+import gc
 
 import numpy as np
 from tqdm import tqdm
 
 from corpus import Corpus
-from vectorize_tokens import matrixify_tokens   
+from vectorize_tokens import matrixify_tokens
 
 if __name__ == '__main__':
     import sys
     _, filename = sys.argv
     corpus = Corpus.connect_to(filename)
-    iterator = tqdm((matrixify_tokens(tokens) for tokens in corpus),
-                    total=len(corpus))
-    np.savez_compressed('corpus', *iterator)
+    array = np.empty(len(corpus), dtype=np.object_)
+
+    for i, tokens in enumerate(tqdm(corpus, total=len(corpus))):
+        array[i] = matrixify_tokens(tokens)
+        if i % 64 == 0:
+            gc.collect(0)
+
+    np.save('corpus', array)
