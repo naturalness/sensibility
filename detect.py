1e1e0e51658c7fac1568d1f426725a48e8d79991
Eddie Antonio Santos <easantos@ualberta.ca>
2016-12-03 15:13:14 -0700

    More random work on detect script.

--- detect.py
+++ detect.py
@@ -21,8 +21,19 @@ import json
 import subprocess
 from pathlib import Path
 
+from keras.models import model_from_json
+
 THIS_DIRECTORY = Path(__file__).parent
 TOKENIZE_JS_BIN = ('node', str(THIS_DIRECTORY / 'tokenize-js'))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', nargs='?', type=Path,
+                    default=Path('/dev/stdin'))
+parser.add_argument('--architecture', type=Path,
+                    default=THIS_DIRECTORY / 'model-architecture.json')
+parser.add_argument('--weights-forwards', type=Path,
+                    default=THIS_DIRECTORY / 'javascript-tiny.5.h5')
+
 
 def tokenize_file(file_obj):
     """
@@ -43,20 +54,36 @@ def tokenize_file(file_obj):
     return json.loads(status.stdout.decode('UTF-8'))
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('filename', nargs='?', type=Path,
-                    default=Path('/dev/stdin'))
-parser.add_argument('--architecture', type=Path,
-                    default=THIS_DIRECTORY / 'model-architecture.json')
-parser.add_argument('--weights-forwards', type=Path,
-                    default=THIS_DIRECTORY / 'javascript-tiny.5.h5')
+class Model:
+    """
+    >>> model = Model.from_filenames(architecture='model-architecture.json',
+    ...                              weights='javascript-tiny.5.h5')
+    """
+    def __init__(self, model):
+        self.model = model
+
+    @classmethod
+    def from_filenames(cls, *, architecture=None, weights=None):
+        with open(architecture) as archfile:
+            model = model_from_json(archfile.read())
+        model.load_weights(weights)
+
+        return cls(model)
+
 
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    with open(str(args.filename), 'rt', encoding='UTF-8') as script:
-        tokens = tokenize_file(script)
-    # TODO: instantiate the model
+    globals().update(vars(parser.parse_args()))
+
+    assert architecture.exists()
+    assert weights_forwards.exists()
+
+    with open(str(filename), 'rt', encoding='UTF-8') as script:
+        raw_tokens = tokenize_file(script)
+
+    forwards = Model.from_filenames(architecture=architecture,
+                                    weights=weights_forwards)
+
     # TODO: create sentences for the file
     # TODO: Predict the next token with the model.
     # TODO: Compare with actual (this is like validation!)
