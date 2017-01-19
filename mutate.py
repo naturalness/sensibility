d71148bfba7636853d6d48cda00c81c0b922b055
Eddie Antonio Santos <easantos@ualberta.ca>
2017-01-19 14:12:35 -0700

    Do a lot of things to the evaluation script...

--- mutate.py
+++ mutate.py
@@ -35,6 +35,7 @@ import argparse
 import sys
 import tempfile
 import random
+from collections import namedtuple
 
 from vocabulary import vocabulary, START_TOKEN, END_TOKEN
 from model_recipe import ModelRecipe
@@ -48,6 +49,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('corpus', type=CondensedCorpus.connect_to)
 parser.add_argument('model', type=ModelRecipe.from_string)
 parser.add_argument('-k', '--mutations', type=int, default=MAX_MUTATIONS)
+parser.add_argument('--headers', action='store_true')
 
 
 def random_token_from_vocabulary():
@@ -59,9 +61,9 @@ def random_token_from_vocabulary():
                           vocabulary.end_token_index - 1)
 
 
-class Model:
+class Sensibility:
     """
-    Base class of the model.
+    A dual-intuition syntax error locator and fixer.
     """
 
 class Mutation:
@@ -69,6 +71,8 @@ class Mutation:
     Base class for all mutations.  Provides methods for comparing and hashing
     mutations.
     """
+
+    __slots__ = ('__weakref__',)
 
     def __eq__(self, other):
         return (
@@ -79,6 +83,15 @@ class Mutation:
 
     def __hash__(self):
         return hash(tuple(getattr(self, attr) for attr in self.__slots__))
+
+    def __repr__(self):
+        cls = type(self).__name__
+        args = ', '.join(repr(getattr(self, attr)) for attr in self.__slots__)
+        return '{}({})'.format(cls, args)
+
+    @property
+    def name(self):
+        return type(self).__name__
 
 
 class Addition(Mutation):
@@ -118,9 +131,16 @@ class Addition(Mutation):
         token = random_token()
         return cls(insertion_point, token)
 
+    @property
+    def location(self):
+        return self.insertion_point
+
 
 class Deletion(Mutation):
     __slots__ = ('index')
+
+    # Only one deletion.
+    token = None
 
     def __init__(self, index):
         self.index = index
@@ -185,6 +205,10 @@ class Substitution(Mutation):
         token = random_token()
         return cls(victim_index, token)
 
+    @property
+    def location(self):
+        return self.index
+
 
 class SourceCode(Mutation):
     """
@@ -204,6 +228,10 @@ class SourceCode(Mutation):
 
     def __len__(self):
         return len(self.tokens)
+
+    @property
+    def inner_length(self):
+        return self.last_index - self.first_index + 1
 
     def random_insertion_point(self, randint=random.randint):
         """
@@ -226,11 +254,11 @@ def test():
     a = Addition.create_random_mutation(program)
     b = Addition.create_random_mutation(program)
     c = Addition.create_random_mutation(program)
+    assert len({a, b, c, c, b, a, c, b}) == 3
 
     d1 = Addition(1, 1)
     d2 = Addition(1, 1)
     s = Substitution(1, 1)
-    assert len({a, b, c, c, b, a, c, b}) == 3
     assert d1 == d2
     assert s != d1
 
@@ -238,37 +266,125 @@ def test():
     mutation.format(program)
 
 
+class Persistence:
+    """
+    Saves A LOT of data to the a CSV file.
+    """
+    def __init__(self, model_recipe):
+        self.corpus = model_recipe.corpus
+        self.fold_no = model_recipe.fold
+        self.epoch = model_recipe.epoch
+        self._file = None
+        self._program = None
+
+    @property
+    def filename(self):
+        return '{corpus}.{fold_no}.{epoch}.csv'.format_map(vars(self))
+
+    headers = (
+        'fold epoch '
+        'file.hash n.tokens '
+        'trial elapsed.time '
+        'mutation mutation.location mutation.token '
+        'fix fix.location fix.token '
+        'rank.correct syntax.ok actual.fix'
+    ).replace(' ', ',')
+
+    @property
+    def program(self):
+        assert self._program_hash
+        return self._program_hash
+
+    @program.setter
+    def program(self, new_program):
+        assert isinstance(new_program, SourceCode)
+        self._program_hash = new_program.hash
+        self._trial = 1
+        self._n_tokens = len(program)
+
+    def add(self, *, mutation=None, fix=None, elapsed_time=None,
+            rank=None, syntax_ok=None, actual_fix=None):
+        assert isinstance(mutation, Mutation)
+        assert isinstance(fix, ...)
+        assert isinstance(elapsed_time, float)
+        assert isinstance(rank, int)
+        assert isinstance(syntax_ok, bool)
+        assert isinstance(actual_fix, bool)
+
+        self._file.writerow((
+            self.fold_no, self.epoch,
+            self.program, self._n_tokens,
+            self._trial, elapsed_time,
+            mutation.name, mutation.location, mutation.token,
+            fix.name, fix.location, fix.token,
+            rank, syntax_ok, actual_fix
+        ))
+        self._trial += 1
+
+    def __enter__(self):
+        assert self._file is None
+        csv_file = open(self.filename, encoding='UTF-8')
+        self._file = csv.writer(csv_file)
+        return self._file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._file.close()
+        self._file = None
+
+
 def main():
     # Requires: corpus, model data (backwards and forwards)
-    model = ...
-    persist = ...
-    for file_hash, tokens in corpus.files_in_fold(fold_no):
-        program = SourceCode(file_hash, tokens)
-        for mutation_kind in Addition, Deletion, Substitution:
-            n_incorrect_files = 0
-            while n_incorrect_files < MAX_MUTATIONS:
-                mutation = mutation_kind.create_random_mutation(program)
-                with tempfile.NamedTemporaryFile(encoding='UTF-8') as mutated_file:
-                    # Apply the mutatation and flush it to disk.
-                    mutation.format(mutation, mutated_file)
-                    mutated_file.flush()
+    args = parser.parse_args()
+    if args.headers:
+        print(Persistence.headers)
+        exit()
 
-                    # TODO: Try the file, reject if it compiles.
-                    if model.is_okay(tempfile.name):
-                        persist.created_correct_file += 1
+    model = args.model
+    print(model)
+    exit(-1)
+    sensibility = Sensibility(args.model)
+
+    with Persistence(model_recipe) as persist:
+        for file_hash, tokens in corpus.files_in_fold(fold_no):
+            program = SourceCode(file_hash, tokens)
+            persist.program = program
+
+            for mutation_kind in Addition, Deletion, Substitution:
+                n_incorrect_files = 0
+                mutations_seen = set()
+                # Clamp down the maximum number of mutations.
+                max_mutations = min(MAX_MUTATIONS, program.inner_length)
+
+                while n_incorrect_files < max_mutations:
+                    mutation = mutation_kind.create_random_mutation(program)
+
+                    if mutation in mutations_seen:
                         continue
+                    else:
+                        mutations_seen.add(mutation)
+                    # TODO: get elapsed time of fix.
+                    with tempfile.NamedTemporaryFile(encoding='UTF-8') as mutated_file:
+                        # Apply the mutatation and flush it to disk.
+                        mutation.format(mutation, mutated_file)
+                        mutated_file.flush()
 
-                    # TODO: save as CSV.
-                    # TODO: Count rank of correct token location
-                    # TODO: Count how many times the suggestion compiles
-                    # TODO: In paper, discuss how the file can still be
-                    # correct, even with a different operation (give example).
-                    # TODO: Count how many times the suggestion IS the true
-                    # result.
+                        # TODO: Try the file, reject if it compiles.
+                        if model.is_okay(tempfile.name):
+                            persist.created_correct_file += 1
+                            continue
 
-                    results = model.detect(tempfile.name)
-                    persist(program, mutation, results)
-                    n_incorrect_files += 1
+                        # TODO: save as CSV.
+                        # TODO: Count rank of correct token location
+                        # TODO: Count how many times the suggestion compiles
+                        # TODO: In paper, discuss how the file can still be
+                        # correct, even with a different operation (give example).
+                        # TODO: Count how many times the suggestion IS the true
+                        # result.
+
+                        results = model.detect(tempfile.name)
+                        persist(program, mutation, results)
+                        n_incorrect_files += 1
+
 
 if __name__ == '__main__':
-    test()
+    main()
