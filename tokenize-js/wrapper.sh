450381b26d88f237ac534532d3ee147c3036bc4a
Eddie Antonio Santos <easantos@ualberta.ca>
2017-02-14 13:34:57 -0700

    Add a wrapper for the tokenizer.

---
+++ tokenize-js/wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Wraps the invocation of index.js such that it automatically NPM installs if
+# there are 
+# Auto npm install before running index.js
+
+original_dir=$(pwd)
+script_dir=$(dirname "$0")
+
+cd "$script_dir" || exit -1
+if [ node_modules -ot package.json ]; then
+    npm install > /dev/null
+fi
+cd "$original_dir" || exit -1
+    
+exec node index.js
