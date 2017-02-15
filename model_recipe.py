8fd9a8f6544e5e8426e07f9471056c881b4c4f8a
Eddie Antonio Santos @ Calzone <easantos@ualberta.ca>
2017-02-15 09:58:34 -0700

    Consolidate the model recipe.

--- model_recipe.py
+++ model_recipe.py
@@ -18,35 +18,18 @@
 
 class ModelRecipe:
     """
-    All the data of an existing model.
+    A filename format for tersely describing a trained file.
+
+    {corpus}-{forward/backward}-{sigmoid}-{sentence}.{fold}.{epoch}.h5
+
+    e.g., javascript-f-300-20.4.3.h5
+
     >>> m = ModelRecipe.from_string('javascript-f-300-20.4.3.h5')
     >>> m.filename
     'javascript-f-300-20.4.3.h5'
     >>> m.next_epoch().filename
     'javascript-f-300-20.4.4.h5'
     """
-
-    @classmethod
-    def from_string(cls, raw_string):
-        try:
-            prefix, fold, epoch, _extension = raw_string.split('.')
-            corpus, direction, sigmoid, sentence = prefix.split('-')
-
-            fold = int(fold)
-            epoch = int(epoch)
-            sigmoid = int(sigmoid)
-            sentence = int(sentence)
-        except ValueError:
-            raise SyntaxError(raw_string)
-
-        if direction == 'f':
-            backwards = False
-        elif direction == 'b':
-            backwards = True
-        else:
-            raise SyntaxError(raw_string)
-
-        return cls(corpus, backwards, sigmoid, sentence, fold, epoch)
 
     def __init__(self, corpus, backwards, sigmoid, sentence, fold, epoch=1):
         self.corpus = corpus
@@ -55,10 +38,6 @@ class ModelRecipe:
         self.sentence = sentence
         self.fold = fold
         self.epoch = epoch
-
-    def next_epoch(self):
-        return type(self)(self.corpus, self.backwards, self.sigmoid,
-                          self.sentence, self.fold, self.epoch + 1)
 
     def __eq__(self, other):
         return (
@@ -75,6 +54,12 @@ class ModelRecipe:
         return 'ModelRecipe.from_string({!r})'.format(self.filename)
 
     @property
+    def filename(self):
+        return (
+            '{s.corpus}-{s.d}-{s.sigmoid}-{s.sentence}.{s.fold}.{s.epoch}.h5'
+        ).format(s=self)
+
+    @property
     def forwards(self):
         return not self.backwards
 
@@ -82,29 +67,14 @@ class ModelRecipe:
     def d(self):
         return 'b' if self.backwards else 'f'
 
-    def flipped(self):
-        """
-        Returns a model recipe with the opposite direction.
-
-        >>> m = ModelRecipe.from_string('javascript-f-300-20.4.3.h5')
-        >>> m.flipped().filename
-        'javascript-b-300-20.4.3.h5'
-        >>> m.flipped().flipped() == m
-        True
-        """
-        return type(self)(self.corpus, not self.backwards, self.sigmoid,
-                          self.sentence, self.fold, self.epoch)
-
-    @property
-    def filename(self):
-        return (
-            '{s.corpus}-{s.d}-{s.sigmoid}-{s.sentence}.{s.fold}.{s.epoch}.h5'
-        ).format(s=self)
-
     def create_batches(self, corpus, batch_size):
         """
         Returns iterables for training and evaluation batches, respectively.
         """
+        from training_utils import LoopBatchesEndlessly, Sentences
+
+        # TODO: CHANGE THIS! THERE ARE NOT 9 TRAINING FOLDS!
+
         # Get the tokens from the 9 training folds.
         training_batches = LoopBatchesEndlessly\
             .for_training(corpus, self.fold,
@@ -123,6 +93,11 @@ class ModelRecipe:
         """
         Creates and compiles the Keras model.
         """
+        from keras.models import Sequential
+        from keras.layers import Dense, Activation
+        from keras.layers import LSTM
+        from keras.optimizers import RMSprop
+
         # Defining the model:
         model = Sequential()
         model.add(LSTM(self.sigmoid,
@@ -139,3 +114,45 @@ class ModelRecipe:
         model = self.create_model()
         model.load_weights(weights_filename)
         return model
+
+    def flipped(self):
+        """
+        Returns a model recipe with the opposite direction.
+
+        >>> m = ModelRecipe.from_string('javascript-f-300-20.4.3.h5')
+        >>> m.flipped().filename
+        'javascript-b-300-20.4.3.h5'
+        >>> m.flipped().flipped() == m
+        True
+        """
+        return type(self)(self.corpus, not self.backwards, self.sigmoid,
+                          self.sentence, self.fold, self.epoch)
+
+    def next_epoch(self):
+        """
+        Return the model recipe for the next epoch. 
+        """
+        return type(self)(self.corpus, self.backwards, self.sigmoid,
+                          self.sentence, self.fold, self.epoch + 1)
+
+    @classmethod
+    def from_string(cls, raw_string):
+        try:
+            prefix, fold, epoch, _extension = raw_string.split('.')
+            corpus, direction, sigmoid, sentence = prefix.split('-')
+
+            fold = int(fold)
+            epoch = int(epoch)
+            sigmoid = int(sigmoid)
+            sentence = int(sentence)
+        except ValueError:
+            raise SyntaxError('Something funky about this string: ' +raw_string)
+
+        if direction == 'f':
+            backwards = False
+        elif direction == 'b':
+            backwards = True
+        else:
+            raise SyntaxError('Could not determine direction: ' + raw_string)
+
+        return cls(corpus, backwards, sigmoid, sentence, fold, epoch)
--- train.py
+++ train.py
@@ -26,6 +26,7 @@ from pathlib import Path
 
 import numpy as np
 
+from model_recipe import ModelRecipe
 from vocabulary import vocabulary
 
 
@@ -35,106 +36,6 @@ SIGMOID_ACTIVATIONS = 300
 
 # This is arbitrary, but it should be fairly small.
 BATCH_SIZE = 512
-
-# {corpus}-{forward/backward}-{sigmoid}-{sentence}.{fold}.{epoch}.h5
-# e.g., javascript-f-300-20.4.3.h5
-
-# TODO: remove this, probably.
-class ModelRecipe:
-    """
-    All the data of an existing model.
-    >>> m = ModelRecipe.create('javascript-f-300-20.4.3.h5')
-    >>> m.filename
-    'javascript-f-300-20.4.3.h5'
-    >>> m.next_epoch().filename
-    'javascript-f-300-20.4.4.h5'
-    """
-
-    @classmethod
-    def from_string(cls, raw_string):
-        try:
-            prefix, fold, epoch, _extension = raw_string.split('.')
-            corpus, direction, sigmoid, sentence = prefix.split('-')
-
-            fold = int(fold)
-            epoch = int(epoch)
-            sigmoid = int(sigmoid)
-            sentence = int(sentence)
-        except ValueError:
-            raise SyntaxError(raw_string)
-
-        if direction == 'f':
-            backwards = False
-        elif direction == 'b':
-            backwards = True
-        else:
-            raise SyntaxError(raw_string)
-
-        return cls(corpus, backwards, sigmoid, sentence, fold, epoch)
-
-    def __init__(self, corpus, backwards, sigmoid, sentence, fold, epoch=1):
-        self.corpus = corpus
-        self.backwards = backwards
-        self.sigmoid = sigmoid
-        self.sentence = sentence
-        self.fold = fold
-        self.epoch = epoch
-
-    def next_epoch(self):
-        return type(self)(self.corpus, self.backwards, self.sigmoid,
-                          self.sentence, self.fold, self.epoch + 1)
-
-    def __repr__(self):
-        return 'ModelRecipe.from_string({!r})'.format(self.filename)
-
-    @property
-    def d(self):
-        return 'b' if self.backwards else 'f'
-
-    @property
-    def filename(self):
-        return (
-            '{s.corpus}-{s.d}-{s.sigmoid}-{s.sentence}.{s.fold}.{s.epoch}.h5'
-        ).format(s=self)
-
-    def create_batches(self, corpus, batch_size):
-        """
-        Returns iterables for training and evaluation batches, respectively.
-        """
-        # Get the tokens from the 9 training folds.
-        training_batches = LoopBatchesEndlessly\
-            .for_training(corpus, self.fold,
-                          batch_size=batch_size,
-                          sentence_length=self.sentence,
-                          backwards=self.backwards)
-        # Get the tokens from the leftover fold (evaluation).
-        eval_batches = LoopBatchesEndlessly\
-            .for_evaluation(corpus, self.fold,
-                          batch_size=batch_size,
-                          sentence_length=self.sentence,
-                          backwards=self.backwards)
-        return training_batches, eval_batches
-
-    def create_model(self):
-        """
-        Creates and compiles the Keras model.
-        """
-        # Defining the model:
-        model = Sequential()
-        model.add(LSTM(self.sigmoid,
-                       input_shape=(self.sentence, len(vocabulary))))
-        model.add(Dense(len(vocabulary)))
-        model.add(Activation('softmax'))
-
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=RMSprop(lr=0.001),
-                      metrics=['categorical_accuracy'])
-        return model
-
-    def create_model_and_load_weights(self, weights_filename):
-        model = self.create_model()
-        model.load_weights(weights_filename)
-        return model
 
 
 def when_none_selected(**kwargs):
@@ -242,15 +143,4 @@ extend.add_argument('weights', type=ModelRecipe.from_string,
 
 if __name__ == '__main__':
     args = parser.parse_args()
-
-    # Load these here, because normally, they take forever...
-    if args.action is not when_none_selected:
-        from keras.models import Sequential
-        from keras.layers import Dense, Activation
-        from keras.layers import LSTM
-        from keras.optimizers import RMSprop
-
-        from training_utils import LoopBatchesEndlessly, Sentences
-        from condensed_corpus import CondensedCorpus
-
     args.action(**vars(args))
