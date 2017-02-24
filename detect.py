7941e1bba4b8c1fae1da14bcae982075782adeb4
Eddie Antonio Santos <easantos@ualberta.ca>
2017-02-24 12:21:21 -0700

    Add try_substitute().

--- detect.py
+++ detect.py
@@ -336,6 +336,18 @@ class Insert(Fix):
         return '\n'.join((msg, line, arrow, suggestion))
 
 
+class Substitute(Fix):
+    name = 'Substitute'
+    def __init__(self, token, pos, tokens):
+        self.token = token
+        assert 1 < pos < len(tokens)
+        self.tokens = tokens
+        self.pos = pos
+
+    def __str__(self):
+        raise NotImplementedError
+
+
 class Fixes:
     def __init__(self, tokens, offset=0):
         self.tokens = tokens
@@ -344,18 +356,33 @@ class Fixes:
 
     def try_remove(self, index):
         pos = index + self.offset
-        print("Will try removing token at", pos, self.tokens[pos])
+        print("Will try removing token at", pos, self.tokens[pos],
+              "originally at ", index)
         suggestion = self.tokens[:pos] + self.tokens[pos + 1:]
         if check_syntax(tokens_to_source_code(suggestion)):
             self.fixes.append(Remove(pos, self.tokens))
+            print("    Success!")
 
     def try_insert(self, index, new_token):
         assert isinstance(new_token, Token)
         pos = index + self.offset
-        print("Will try inserting", new_token, " at", pos)
+        print("Will try inserting", new_token, " at", pos,
+              "originally at ", index)
         suggestion = self.tokens[:pos] + [new_token] + self.tokens[pos:]
         if check_syntax(tokens_to_source_code(suggestion)):
             self.fixes.append(Insert(new_token, pos, self.tokens))
+            print("    Success!")
+
+    def try_substitute(self, index, new_token):
+        assert isinstance(new_token, Token)
+        pos = index + self.offset
+        print("Will try substituting", new_token, " for ",
+              self.tokens[pos], "at", pos,
+              "originally at ", index)
+        suggestion = self.tokens[:pos] + [new_token] + self.tokens[pos + 1:]
+        if check_syntax(tokens_to_source_code(suggestion)):
+            self.fixes.append(Substitute(new_token, pos, self.tokens))
+            print("    Success!")
 
     def __bool__(self):
         return len(self.fixes) > 0
