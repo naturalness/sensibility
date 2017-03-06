231c687aba08ccda8256a18479b29e7ad7d275e6
Eddie Antonio Santos <easantos@ualberta.ca>
2017-03-06 16:35:06 -0700

    More or less get the directory meta-files in the right order.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # -*- coding: UTF-8 -*-
 from setuptools import setup, find_packages
+from os import path
 
+here = path.abspath(path.dirname(__file__))
 
 def slurp(filename):
-    with open(filename) as text_file:
+    with open(filename, encoding='UTF-8') as text_file:
         return text_file.read()
 
 
@@ -12,7 +14,7 @@ setup(
     version='0.2.dev0',
 
     description='Syntax error detecter and fixer',
-    long_description=slurp('README.md'),
+    long_description=slurp(path.join(here, 'README.md')),
     url='https://github.com/eddieantonio/training-grammar-guru',
 
     author='Eddie Antonio Santos',
