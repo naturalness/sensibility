dbf1fc6bf4523aeea30d3f9d554d3f5bb748c8fc
Eddie Antonio Santos @ Calzone <easantos@ualberta.ca>
2017-03-08 11:22:21 -0700

    So I learned that POSIX sh's test doesn't have the -ot comparison...

--- sensibility/tokenize-js/wrapper.sh
+++ sensibility/tokenize-js/wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Wraps the invocation of index.js such that it automatically NPM installs if
 # there are 
@@ -8,7 +8,7 @@ original_dir=$(pwd)
 script_dir=$(dirname "$0")
 
 cd "$script_dir" || exit -1
-if [ node_modules -ot package.json ]; then
+if [[ node_modules -ot package.json ]]; then
     npm install > /dev/null
 fi
 cd "$original_dir" || exit -1
