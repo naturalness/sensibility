55b9c142f53358f7da694e0ac791ae78bee0e7d1
Eddie Antonio Santos <easantos@ualberta.ca>
2017-05-08 14:15:23 -0600

    Let connection take over for older version.

--- sensibility/connection.py
+++ sensibility/connection.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+# Copyright 2017 Eddie Antonio Santos <easantos@ualberta.ca>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 from types import ModuleType
 
@@ -10,6 +24,9 @@ import github3
 
 
 class ConnectionModule(ModuleType):
+    """
+    Returns the appropriate clients.
+    """
     __all__ = ['redis_client', 'sqlite3_connection', 'github']
 
     @property
