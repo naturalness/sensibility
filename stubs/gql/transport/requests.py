5436fb2305df5a1a07f478a563da96962d41431d
Eddie Antonio Santos <easantos@ualberta.ca>
2017-05-09 15:29:21 -0600

    Prompty remove it.

--- stubs/gql/transport/requests.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-from . import Transport
-
-class RequestsHTTPTransport(Transport):
-    def __init__(self, url: str , auth=None, use_json: bool=False, timeout=None, **kwargs) -> None: ...
