a4093687102c89153f4fadf2fb183f03fcff29e8
Eddie Antonio Santos <easantos@ualberta.ca>
2017-10-17 08:57:21 -0600

    Add learning rate as a bin argument.

--- sensibility/model/lstm/train.py
+++ sensibility/model/lstm/train.py
@@ -194,7 +194,7 @@ class ModelDescription:
         model.add(Activation('softmax'))
 
         model.compile(loss='categorical_crossentropy',
-                      optimizer=RMSprop(lr=self.learning_rate, clipnorm=1.),
+                      optimizer=RMSprop(lr=self.learning_rate),
                       metrics=['categorical_accuracy'])
         return model
 
