3f39f0a02119ed70dd41bb297076bb9596dee58d
Eddie Antonio Santos <easantos@ualberta.ca>
2017-10-19 10:42:49 -0600

    Use alternate method to calculate distance.

--- sensibility/evaluation/distance.py
+++ sensibility/evaluation/distance.py
@@ -19,7 +19,7 @@
 Determines the Levenshtein distance between two source files.
 """
 
-from typing import Iterable, Optional, Sequence, cast
+from typing import Iterable, Optional, Sequence, Tuple, cast
 
 from Levenshtein import distance, editops  # type: ignore
 from edit_distance import SequenceMatcher  # type: ignore
@@ -32,6 +32,9 @@ from sensibility.abram import at_least
 
 # Supplementary Private Use Area B
 PUA_B_START = 0x100000
+
+
+EditOp = Tuple[str, int, int, int, int]
 
 
 def encode(entries: Iterable[str]) -> str:
@@ -60,21 +63,6 @@ def tokenwise_distance(file_a: bytes, file_b: bytes, abstract_open_classes=True)
         seq_b = to_value_stream(file_b)
     return SequenceMatcher(a=seq_a, b=seq_b).distance()
 
-
-def to_abstraced_tokens(source: bytes) -> Sequence[str]:
-    """
-    Turns the source code to a sequence of vocabulary tokens.
-    """
-    return tuple(language.vocabularize(source))
-
-
-def to_value_stream(source: bytes) -> Sequence[str]:
-    """
-    Turns the source code to sequence of tokens values.
-    """
-    return tuple(token.value for token in language.tokenize(source))
-
-
 class FixEvent:
     def __init__(self, fix: Edit, line_no: int) -> None:
         # Feature request: use the current language for each fix, then to
@@ -99,18 +87,31 @@ class FixEvent:
         return language.vocabulary.to_text(new) if new is not None else None
 
 
-def determine_edit(file_a: bytes, file_b: bytes) -> Edit:
-    return determine_fix_event(file_a, file_b).fix
+def determine_edit(file_a: bytes, file_b: bytes, abstract_open_classes=True) -> Edit:
+    return determine_fix_event(file_a, file_b, abstract_open_classes).fix
 
 
-def determine_fix_event(file_a: bytes, file_b: bytes) -> FixEvent:
+def differences_only(ops) -> Sequence[EditOp]:
+    return tuple(op for op in ops if op[0] != 'equal')
+
+
+def determine_fix_event(file_a: bytes, file_b: bytes, abstract_open_classes=True) -> FixEvent:
     """
     For two source files with Levenshtein distance of one, this returns the
     edit that converts the first file into the second file.
     """
-    src_locs, src = lists(language.vocabularize_with_locations(file_a))
-    dest = list(language.vocabularize(file_b))
-    ops = editops(encode(src), encode(dest))
+    if abstract_open_classes:
+        src_locs, src = lists(language.vocabularize_with_locations(file_a))
+        dest = to_abstraced_tokens(file_b)
+    else:
+        src_locs = []
+        src = []
+        for token in language.tokenize(file_a):
+            src_locs.append(token.location)
+            src.append(token.value)
+        dest = to_value_stream(file_b)
+
+    ops = differences_only(SequenceMatcher(a=src, b=dest).get_opcodes())
     # This only works for files with one edit!
     assert len(ops) == 1
 
@@ -119,22 +120,43 @@ def determine_fix_event(file_a: bytes, file_b: bytes) -> FixEvent:
     to_index = language.vocabulary.to_index_or_unk
 
     # Decode editop's format into our "Database-friendly" format.
-    (type_name, src_pos, dest_pos), = ops
+    (type_name, src_pos, src_end, dest_pos, dest_end), = ops
+    assert src_end in (src_pos, src_pos + 1)
+    assert dest_end in (dest_pos, dest_pos + 1)
     if type_name == 'insert':
         code, new, old = 'i', to_index(dest[dest_pos]), None
+        original_token_index = src_pos
+        edit_index = dest_pos
     elif type_name == 'delete':
         code, new, old = 'x', None, to_index(src[src_pos])
+        original_token_index = edit_index = src_pos
     elif type_name == 'replace':
         code, new, old = 's', to_index(dest[dest_pos]), to_index(src[src_pos])
+        original_token_index = edit_index = src_pos
     else:
         raise ValueError(f'Cannot handle operation: {ops}')
 
     # Note: in the case of insertions, src_pos will be the index to insert
     # BEFORE, which is exactly what Edit.deserialize wants; src_position also
     # works for deletions, and substitutions.
-    edit = Edit.deserialize(code, src_pos, new, old)
-    error_token = src_locs[at_least(0, src_pos - 1) if code == 'i' else src_pos]
+    edit = Edit.deserialize(code, edit_index, new, old)
+    error_token = src_locs[original_token_index]
+
     return FixEvent(edit, error_token.line)
+
+
+def to_abstraced_tokens(source: bytes) -> Sequence[str]:
+    """
+    Turns the source code to a sequence of vocabulary tokens.
+    """
+    return tuple(language.vocabularize(source))
+
+
+def to_value_stream(source: bytes) -> Sequence[str]:
+    """
+    Turns the source code to sequence of tokens values.
+    """
+    return tuple(token.value for token in language.tokenize(source))
 
 
 def lists(it):
