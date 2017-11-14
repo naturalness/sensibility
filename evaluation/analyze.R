7e47126bdcb6997936497cdb73d15ef9f66c6fc7
Eddie Antonio Santos <easantos@ualberta.ca>
2017-11-14 13:44:10 -0700

    Better Ring.

--- evaluation/analyze.R
+++ evaluation/analyze.R
@@ -12,49 +12,50 @@ results <- dbGetQuery(con, "
 ")
 dbDisconnect(con)
 
-attach(results)
-
-# Treat these as factors rather than continuous variables
-results$dropout <- as.factor(dropout)
-results$patience <- as.factor(patience)
-
-
+# Calculate the reciprocal of the rank. Nulls are converted to zeros.
 reciprank <- function (vec) {
   rrs <- 1.0 / vec
   rrs[is.na(rrs)] = 0
   return(rrs)
 }
 
-# Get reciprocal ranks.
-results$line_location_rr <- reciprank(line_location_rank)
-results$exact_location_rr <- reciprank(exact_location_rank)
-results$valid_fix_rr <- reciprank(valid_fix_rank)
-results$true_fix_rr <- reciprank(true_fix_rank)
-
-# Attach to new columns without invoking warnings:
-# Can't use with()/within() because I want to create variables outside
-# of the environment
-detach(results)
-attach(results)
+# Apply some transformations to the raw results.
+results <- within(results, {
+  # Treat these as factors rather than continuous variables
+  dropout <- as.factor(dropout)
+  patience <- as.factor(patience)
+  partition <- as.factor(partition)
+  
+  # Get reciprocal ranks.
+  line_location_rr <- reciprank(line_location_rank)
+  exact_location_rr <- reciprank(exact_location_rank)
+  valid_fix_rr <- reciprank(valid_fix_rank)
+  true_fix_rr <- reciprank(true_fix_rank)
+  
+  # Figure out the mean validation loss
+  mean_val_loss <- (forwards_val_loss + backwards_val_loss)/2
+})
 
 # Get the MRR for all these responding variables.
-aggdata <- aggregate(
+aggdata <- with(results, {aggregate(
   # These are the responding variables that should be meaned.
-  cbind(line_location_rr, exact_location_rr, valid_fix_rr, true_fix_rr,
-        forwards_val_loss, backwards_val_loss) ~
+  cbind(line_location_rr, exact_location_rr, valid_fix_rr, true_fix_rr, mean_val_loss) ~
   # These are the manipulated variables; there may be more!
     hidden_layers + context_length + dropout + patience + optimizer + partition,
   results, mean
-)
+)})
 
 # Figure out what actually affects line location MRR
-line.model <- lm(exact_location_rr ~
-                   hidden_layers + context_length + dropout + patience + optimizer + partition)
-
-detach(results)
+line.model <- with(results, {lm(
+  exact_location_rr ~
+    hidden_layers + context_length + dropout + patience + optimizer + partition + mean_val_loss
+)})
+summary(line.model)
 
 # Is exact location correlated with the validation loss?
-with(aggdata, cor((forwards_val_loss + backwards_val_loss) / 2, exact_location_rr))
+with(aggdata, cor.test(mean_val_loss, exact_location_rr))
+# How about true fix?
+with(aggdata, cor.test(mean_val_loss, true_fix_rr))
 
 # Sample violin plot:
-ggplot(results, aes(x=optimizer, y=line_location_rr)) + geom_violin
+ggplot(results, aes(x=optimizer, y=exact_location_rr)) + geom_violin()
