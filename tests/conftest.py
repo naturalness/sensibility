58c4e09fc7b9c187259c176e1cd289ec73737f9f
Eddie Antonio Santos <easantos@ualberta.ca>
2017-11-20 15:49:14 -0700

    Make JavaScript an opt-in feature.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,8 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      help="run slow tests")
+    parser.addoption("--with-javascript", action="store_true",
+                     help="run JavaScript tests")
 
 
 @pytest.fixture
