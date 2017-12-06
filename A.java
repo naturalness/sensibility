347349252e3a31919e93218aa5ce57d9a8d6fcbe
Eddie Antonio Santos <easantos@ualberta.ca>
2017-12-06 12:54:47 -0700

    Do not track any demo .java files in root dir.

--- A.java
+++
@@ -1,9 +0,0 @@
-public class A {
-    public static void main(String args[]) {
-        if (args.length < 2) {
-            System.out.println("Not enough args!");
-            System.exit(1);
-        }
-        System.out.println("Hello, world!");
-    }
-}
