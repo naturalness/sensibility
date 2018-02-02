8aa61d79bceba5071bb7f00009313a7fc22deb57
Eddie Antonio Santos <easantos@ualberta.ca>
2018-02-02 11:20:41 -0700

    Who doesn't love random flailing in R?

--- evaluation/analyze-original.R
+++ evaluation/analyze-original.R
@@ -54,7 +54,7 @@ if (!is.na(TRAINING_SET_SIZE)) {
 }
 
 # Get the MRR for all these responding variables.
-aggdata <- with(results, {aggregate(
+aggdata.partitioned <- with(results, {aggregate(
   # These are the responding variables that should be meaned.
   cbind(line_location_rr, exact_location_rr, valid_fix_rr, true_fix_rr, mean_val_loss) ~
   # These are the manipulated variables; there may be more!
@@ -62,12 +62,39 @@ aggdata <- with(results, {aggregate(
   results, mean
 )})
 
+aggdata <- with(results, {aggregate(
+  # These are the responding variables that should be meaned.
+  cbind(line_location_rr, exact_location_rr, valid_fix_rr, true_fix_rr, mean_val_loss) ~
+    # These are the manipulated variables; there may be more!
+    hidden_layers + context_length + patience,
+  results, mean
+)})
+
+MAX_RANK <- 5
+
 results.300 <- subset(results, hidden_layers == 300)
-ggplot(results.300, aes(x=hidden_layers, y=valid_fix_rr)) + geom_violin()
-ggplot(results.300, aes(x=hidden_layers, y=exact_location_rr)) + geom_violin()
-ggplot(results.300, aes(x=hidden_layers, y=true_fix_rr)) + geom_violin()
+results.300 <- within(results.300, {
+  true_fix_rank[is.na(true_fix_rank)] <- Inf
+  true_fix_rank[true_fix_rank >= MAX_RANK] <- MAX_RANK
+  # Convert into an ordinal factor
+  true_fix_rank <- ordered(true_fix_rank,
+                           levels=1:MAX_RANK,
+                           labels=c("1", "2", "3", "4", "5+"))
+})
+
+amount.first <- length(which(results.300$true_fix_rank == 1))
+pct.first <- 100 * amount.first / nrow(results.300)
+
+ggplot(results.300, aes(x=true_fix_rank)) +
+  geom_bar()
+
+#ggplot(results.300, aes(x=hidden_layers, y=valid_fix_rr)) + geom_violin()
+#ggplot(results.300, aes(x=hidden_layers, y=exact_location_rr)) + geom_violin()
+
 
 mean(results.300$line_location_rr)
 mean(results.300$exact_location_rr)
 mean(results.300$valid_fix_rr)
 mean(results.300$true_fix_rr)
+
+
